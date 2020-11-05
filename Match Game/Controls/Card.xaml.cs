139230b69b9b22c3922b28b7710a9e40bf59c1ed
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-11-05 08:12:45 -0700

    Created the Match Game project

---
+++ Match Game/Controls/Card.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Match_Game.Controls
+{
+    public partial class Card : UserControl, INotifyPropertyChanged
+    {
+        public enum eState
+        {
+            Inactive,
+            Idle,
+            Flipped,
+            Matched
+        }
+
+        public GameWindow Owner { get; set; }
+        private eState state = eState.Inactive;
+        public eState State 
+        { 
+            get { return state; } 
+            set
+            {
+                if (value != state)
+                {
+                    state = value;
+                    Interactable = (state == eState.Idle);
+                    Show = (state == eState.Flipped || state == eState.Matched);
+                    NotifyPropertyChanged("State");
+                }
+            }
+        }
+        public bool Show 
+        { 
+            set
+            {
+                if (value)
+                {
+                    lblSymbol.Visibility = Visibility.Visible;
+                    imgCard.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    lblSymbol.Visibility = Visibility.Hidden;
+                    imgCard.Visibility = Visibility.Visible;
+                }
+            }
+        }
+        private bool interactable;
+        public bool Interactable 
+        { 
+            get { return interactable; }
+            set
+            {
+                if (value != interactable)
+                {
+                    interactable = value;
+                    NotifyPropertyChanged("Interactable");
+                }
+            }
+        }
+        private string symbol;
+        public string Symbol 
+        { 
+            get { return symbol; }
+            set
+            {
+                if (value != symbol)
+                {
+                    symbol = value;
+                    NotifyPropertyChanged("Symbol");
+                }
+            }
+        }
+
+        public Card()
+        {
+            InitializeComponent();
+            DataContext = this;
+            this.Loaded += Card_Loaded;
+        }
+
+        private void Card_Loaded(object sender, RoutedEventArgs e)
+        {
+            Owner = (GameWindow)Window.GetWindow(this);
+            Owner.RegisterCard(this);
+        }
+
+        protected void NotifyPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void btnCard_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer("../../../Resources/card.wav");
+            player.Load();
+            player.Play();
+            State = eState.Flipped;
+            Owner.SelectCard(this);
+        }
+    }
+}
---
+++ Match Game/GameWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Match_Game.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Match_Game
+{
+    public partial class GameWindow : Window
+    {
+        private Random gen = new Random();
+        private DispatcherTimer timer = new DispatcherTimer();
+        private List<Card> cards = new List<Card>();
+        private List<string> symbols = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",",
+            "b", "b", "v", "v", "w", "w",
+        };
+
+        public int Tries { get; set; } = 0;
+        private Card card1 = null;
+        private Card card2 = null;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerTick;
+        }
+
+        public void RegisterCard(Card card)
+        {
+            card.State = Card.eState.Idle;
+            int index = gen.Next(symbols.Count);
+            card.Symbol = symbols[index];
+            symbols.RemoveAt(index);
+            cards.Add(card);
+        }
+
+        public void SelectCard(Card card)
+        {
+            if (card1 == null)
+            {
+                // Set card1 to card
+                card1 = card;
+            }
+            else
+            {
+                // Set card2 to card
+                Tries++;
+                card2 = card;
+
+                if (card1.Symbol == card2.Symbol) // Check for Matching Cards (card1, card2). Compare card Symbols
+	            {
+                    // Set card1 and card2 State to Matched; Set card1 and card2 to null
+                    card1.State = Card.eState.Matched;
+                    card2.State = Card.eState.Matched;
+                    card1 = null;
+                    card2 = null;
+                }
+
+                else
+                {
+                    // Disable All Cards, so They Can’t be Selected for a Second
+                    // This Allows the Player to see the Flipped Card for a Moment
+                    foreach (var c in cards) // Iterate Through All Cards
+		            {
+                        if (c.State == Card.eState.Idle)// If card State is Idle
+                        {
+                            // Set card State to Inactive
+                            c.State = Card.eState.Inactive;
+                        }
+                    }
+                    // Start timer, After a Second the TimerTick is Called and the Cards Reset
+                    timer.Start();
+                    timer.Tick += TimerTick;
+                }
+            }
+            txtTries.Text = "Tries: " + Tries.ToString();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            foreach (var c in cards)
+            {
+                if (c.State != Card.eState.Matched)
+                {
+                    c.State = Card.eState.Idle;
+                }
+            }
+            card1 = null;
+            card2 = null;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            var currentExecutablePath = Process.GetCurrentProcess().MainModule.FileName;
+            Process.Start(currentExecutablePath);
+            Application.Current.Shutdown();
+        }
+    }
+}
