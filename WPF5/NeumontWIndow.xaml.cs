e4d41919f92f2375c9261feaff8e5d4f8e0ec000
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-11-10 02:01:58 -0700

    Added WPF5

---
+++ WPF5/NeumontWIndow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF5
+{
+    /// <summary>
+    /// Interaction logic for NeumontWIndow.xaml
+    /// </summary>
+    public partial class NeumontWindow : Window
+    {
+        public NeumontWindow()
+        {
+            InitializeComponent();
+
+            List<Cohort> cohorts = new List<Cohort>();
+            Cohort cohort35 = new Cohort() { Name = "Cohort 35" };
+            cohort35.Students.Add(new Student() { Name = "Ana", ID = 34 });
+            cohort35.Students.Add(new Student() { Name = "Dakota", ID = 23 });
+            cohort35.Students.Add(new Student() { Name = "Cameron", ID = 12 });
+            cohorts.Add(cohort35);
+
+            Cohort cohort34 = new Cohort() { Name = "Cohort 34" };
+            cohort34.Students.Add(new Student() { Name = "Ryder", ID = 78 });
+            cohort34.Students.Add(new Student() { Name = "Adam", ID = 32 });
+            cohorts.Add(cohort34);
+        }
+
+        public class Cohort
+        {
+            public string Name { get; set; }
+            public ObservableCollection<Student> Students { get; set; }
+
+            public Cohort()
+            {
+                Students = new ObservableCollection<Student>();
+            }
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+
+        }
+    }
+}
---
+++ WPF5/TreeViewWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF5
+{
+    
+    public partial class TreeViewWindow : Window
+    {
+        public TreeViewWindow()
+        {
+            InitializeComponent();
+
+            Uri uri = new Uri("pack://application:,,,/Resources/sus.bmp");
+            BitmapImage image = new BitmapImage(uri);
+
+            MenuItem root = new MenuItem() { Title = "Root", Icon = image };
+            MenuItem child1 = new MenuItem() { Title = "Child 1", Icon = image };
+            child1.Items.Add(new MenuItem { Title = "Child 1.1", Icon = image });
+            child1.Items.Add(new MenuItem { Title = "Child 1.2", Icon = image });
+            root.Items.Add(child1);
+            root.Items.Add(new MenuItem { Title = "Child 2", Icon = image });
+
+            trvMenu.Items.Add(root);
+        }
+    }
+
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public BitmapImage Icon { get; set; }
+        public ObservableCollection<MenuItem> Items { get; set; }
+
+        public MenuItem()
+        {
+            Items = new ObservableCollection<MenuItem>();
+        }
+    }
+}
